4ba84af9d488131c9898d5466976caabd23e5c14
quieoo <715094648@qq.com>
2022-07-19 16:25:49 +0800

    dpdk_utils

--- examples/skeleton/utils.h
+++ examples/skeleton/utils.h
@@ -1,7 +1,10 @@
+#ifndef COMMON_UTILS_H_
+#define COMMON_UTILS_H_
 
+#define APP_EXIT(format, args...) do{\
+	printf("APP_EXIT: ");\
+	printf(format,args);\
+	exit(1);\
+} while(0)
 
-void APP_EXIT(format,...){
-	printf("APP_EXIT: ");
-	printf(format);
-	exit(1);
-}
+#endif /* COMMON_UTILS_H_ */
