e16fa021a69993f68ccbe1360c5a2341a789ce9b
quieoo <715094648@qq.com>
2022-07-19 16:07:02 +0800

    dpdk_utils

--- examples/skeleton/dpdk_utils.c
+++ examples/skeleton/dpdk_utils.c
@@ -3,9 +3,26 @@
 void dpdk_init(struct application_dpdk_config *app_dpdk_config){
     int ret = 0;
 
+    /* Check that DPDK enabled the required ports to send/receive on */
+
     ret = rte_eth_dev_count_avail();
-	printf("``available devices: %d\n",ret);
-    if (app_dpdk_config->port_config.nb_ports > 0 && ret != app_dpdk_config->port_config.nb_ports)
+	if (app_dpdk_config->port_config.nb_ports > 0 && ret != app_dpdk_config->port_config.nb_ports)
 		APP_EXIT("Application will only function with %u ports, num_of_ports=%d",app_dpdk_config->port_config.nb_ports, ret);
     
+    /* Check for available logical cores */
+    ret = rte_lcore_count();
+	if (app_dpdk_config->port_config.nb_queues > 0 && ret < app_dpdk_config->port_config.nb_queues)
+		APP_EXIT("At least %u cores are needed for the application to run, available_cores=%d",
+			 app_dpdk_config->port_config.nb_queues, ret);
+	else
+		app_dpdk_config->port_config.nb_queues = ret;
+    
+    if (app_dpdk_config->reserve_main_thread)
+		app_dpdk_config->port_config.nb_queues -= 1;
+
+
+    
+
+
 }
+
