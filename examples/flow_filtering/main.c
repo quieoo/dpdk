c28e5485ebeb7254c8830460f1fa930407e83b44
quieoo <715094648@qq.com>
2022-09-26 10:50:17 +0800

    t

--- examples/flow_filtering/main.c
+++ examples/flow_filtering/main.c
@@ -97,7 +97,7 @@ main_loop(void)
 				for (j = 0; j < nb_rx; j++) {
 					struct rte_mbuf *m = mbufs[j];
 					get_and_print_ip4(m);
-					//get_and_print_eth(m);
+					get_and_print_eth(m);
 					rte_pktmbuf_free(m);
 				}
 			}
